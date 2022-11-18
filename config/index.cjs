7e9c840b2943ac29700f8891ae519343433ea3b5
zhangjia20010406 <118338379+zhangjia20010406@users.noreply.github.com>
2022-11-18 20:08:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,59 +22,6 @@ USE_PASSAGE: 'push-deer',
       name: '飞飞小朋友',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU17984T4giFs02wa40JHkgS960xbtprrOnb7eMI',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '02-12',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-       // {
-         // type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-       // },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '飞飞小盆友', year: '2002', date: '02-12',
-        },
-       // {
-          //type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        //},
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2021-09-16' },
-        // 结婚纪念日
-        //{ keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  ],
-
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0002',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU17918T3nVrzHRQwsDqNwOVdrg0eD5dK8UkTVId',
-    }
-  ],
-
-}
-
-module.exports = USER_CONFIG
-// 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
-
-  PROVINCE: '山东',
-  CITY: '济南',
-
-  USERS: [
-    {
-      // 想要发送的人的名字
-      name: '飞飞小朋友',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU17918T3nVrzHRQwsDqNwOVdrg0eD5dK8UkTVId',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
@@ -118,5 +65,3 @@ module.exports = USER_CONFIG
 }
 
 module.exports = USER_CONFIG
-
-
